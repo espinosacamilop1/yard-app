f7c83b6ed69bee5dba2c04f1aa518b3aacceac88
espinosacamilop1 <63834682+espinosacamilop1@users.noreply.github.com>
2023-09-06 17:56:48 -0400

    Update index.js

--- pages/api/clients/index.js
+++ pages/api/clients/index.js
@@ -2,29 +2,41 @@ import clientPromise from '<src>/lib/mongo'
 import { MongoClient } from 'mongodb';
 
 
-    const client = await clientPromise;
-    const db = await client.db("Clients");
+
 
 const handler = async (req, res) => {
-
-    
-    console.log(client)
-    switch (req.method) {
-        case "POST":
-          let bodyObject = JSON.parse(req.body);
-          let myPost = await db.collection("clients").insertOne(bodyObject);
-          res.json(myPost.ops[0]);
-          break;
-        case "GET":
+    const client = await clientPromise;
+  if (req.method === 'GET') {
+      try {
+          const db = await client.db('Clients');
           const allClients = await db.collection("clients").find({}).toArray();
-          res.statusCode = 200
           res.setHeader('Content-Type', 'application/json')
           res.end(JSON.stringify({ status: 200, data: allClients }));
-          break;
-        default:
-          res.setHeader("Allow", ["GET"])
-          res.status(425).end(`Method ${req.method} is not allowed`)
       }
+  } else {
+                res.setHeader("Allow", ["GET"])
+          res.status(425).end(`Method ${req.method} is not allowed`)
+  }
+    
+
+    
+    // console.log(client)
+    // switch (req.method) {
+    //     case "POST":
+    //       let bodyObject = JSON.parse(req.body);
+    //       let myPost = await db.collection("clients").insertOne(bodyObject);
+    //       res.json(myPost.ops[0]);
+    //       break;
+    //     case "GET":
+    //       const allClients = await db.collection("clients").find({}).toArray();
+    //       res.statusCode = 200
+    //       res.setHeader('Content-Type', 'application/json')
+    //       res.end(JSON.stringify({ status: 200, data: allClients }));
+    //       break;
+    //     default:
+    //       res.setHeader("Allow", ["GET"])
+    //       res.status(425).end(`Method ${req.method} is not allowed`)
+    //   }
 }
 
 export default handler
