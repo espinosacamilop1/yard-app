9bbafeb40be9d0f30e5660642eba9dcf6e9f33d1
espinosacamilop1 <63834682+espinosacamilop1@users.noreply.github.com>
2023-09-06 17:01:54 -0400

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,1 +1,6 @@
 
+export default (req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ message: 'Hello from the Daily route' }))
+}
