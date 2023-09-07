6107a3ec33beba888e56052b8d77c2038ca1d859
espinosacamilop1 <espinosacamilop1@gmail.com>
2023-09-06 22:33:40 -0400

    added function

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -4,7 +4,12 @@ import { Inter } from 'next/font/google'
 import styles from '<src>/styles/Home.module.css'
 import AdminDashboard from '../components/Dashboard';
 
-
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/clients')
+  const fetchedClients = await res.json();
+  console.log('this is fetched clients: ', fetchedClients)
+  return { props: { fetchedClients } }
+}
 
 export default function Dashboard({fetchedClients}) {
     return (
