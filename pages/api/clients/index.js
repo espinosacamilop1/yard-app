ca15199fcd86ab45ea36efef25898cf8ee019511
espinosacamilop1 <63834682+espinosacamilop1@users.noreply.github.com>
2023-09-06 21:44:21 -0400

    Update index.js

--- pages/api/clients/index.js
+++ pages/api/clients/index.js
@@ -5,43 +5,22 @@ import { MongoClient } from 'mongodb';
 
 
 const handler = async (req, res) => {
-    const client = await clientPromise;
-    const db = await client.db('Clients');
 
-  // if (req.method === 'GET') {
-  //     try {
-  //         const db = await client.db('Clients');
-  //         const allClients = await db.collection("clients").find({}).toArray();
-  //         res.setHeader('Content-Type', 'application/json')
-  //         res.end(JSON.stringify({ status: 200, data: allClients }));
-  //     } catch(error) {
-  //     console.error('Error updating clients:', error);
-  //     res.status(500).json({ error: 'Internal Server Error' });
-  //     }
-  // } else {
-  //               res.setHeader("Allow", ["GET"])
-  //         res.status(425).end(`Method ${req.method} is not allowed`)
-  // }
-    
+      try {
+       const client = await clientPromise;
+       const db = client.db("Clients");
 
-    
-    console.log(client)
-    switch (req.method) {
-        case "POST":
-          let bodyObject = JSON.parse(req.body);
-          let myPost = await db.collection("clients").insertOne(bodyObject);
-          res.json(myPost.ops[0]);
-          break;
-        case "GET":
-          const allClients = await db.collection("clients").find({}).toArray();
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'application/json')
-          res.end(JSON.stringify({ status: 200, data: allClients }));
-          break;
-        default:
-          res.setHeader("Allow", ["GET"])
-          res.status(425).end(`Method ${req.method} is not allowed`)
-      }
+       const clients = await db
+           .collection("clients")
+           .find({})
+           .toArray();
+
+       res.json(movies);
+   } catch (e) {
+       console.error(e);
+   }
+
+  
 }
 
 export default handler
